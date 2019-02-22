a1380800c4655ae3c3f173fddb46fe11aec1fdbe
myDeveloperJourney <daniel@mydeveloperjourney.com>
2019-02-22 16:15:15 -0600

    builds Dashboard component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Dashboard = (props) => (
+    <div>
+        <h5>Here are your todos</h5>
+        <div>
+            {
+                /* We'll add our Todos here */
+            }
+        </div>
+        <form>
+            <input />
+            <button>Add Todo</button>
+        </form>
+    </div>
+)
+
+export default Dashboard
