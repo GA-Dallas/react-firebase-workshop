8bf647661eca299eac2a615b3983a788beb530a1
myDeveloperJourney <daniel@mydeveloperjourney.com>
2019-02-22 16:14:46 -0600

    imports Dashboard component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Dashboard from './components/Dashboard'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <h1>Welcome to React Fire Todos</h1>
+        <Dashboard />
       </div>
     );
   }
